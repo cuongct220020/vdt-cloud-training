d598c1fc79d726cac950f43497099a1f0765e919
Cuongit1-hust <cuongct0902@gmail.com>
2026-02-17 00:14:31 +0700

    feat: reverse string using stack

--- bash/A-tier/reverse-string/reverse_string.sh
+++ bash/A-tier/reverse-string/reverse_string.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 main () {
-  rev <<< "$1"
+    input_string=$1
+
+    if [ -z "$input_string" ]; then
+        echo "Input is not available..."
+        exit 1
+    fi
+
+    local string_length=${#input_string}
+    local stack=()
+
+    for (( i=0; i < string_length; i++ )); do
+        char=${input_string:i:1}
+        stack+=("$char")
+    done
+
+    local last_index=$(( ${#stack[@]} - 1 ))
+    for (( i=last_index; i >= 0; i-- )); do
+        echo -n "${stack[i]}"
+    done
+
+    echo ""
 }
 
 main "$@"
